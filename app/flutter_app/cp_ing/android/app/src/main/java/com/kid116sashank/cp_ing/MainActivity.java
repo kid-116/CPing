97e3a22babd0bf22c1454e0cc35cd9bf337b654e
kid-116 <mehultodi116@gmail.com>
2021-09-04 12:38:13 +0530

    started blank projects

---
+++ app/flutter_app/cp_ing/android/app/src/main/java/com/kid116sashank/cp_ing/MainActivity.java
@@ -0,0 +1,6 @@
+package com.kid116sashank.cp_ing;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ app/web_api/app.py
@@ -0,0 +1,7 @@
+import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
