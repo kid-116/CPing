394ff22b5eb0505625c5199aabe6006358bd6f53
kid-116 <mehultodi116@gmail.com>
2021-09-04 17:21:43 +0530

    implemented webscraping for codechef

--- app/web_api/app.py
+++ app/web_api/app.py
@@ -1,7 +1,20 @@
-import flask
-from flask import Flask
+from flask import Flask, jsonify
+from webscraping.codechef.script import main as scrape_codechef
 
 app = Flask(__name__)
+
+@app.route('/api/codechef/contests/', methods=['GET'])
+def get_codechef_contests():
+    contests = scrape_codechef()
+    return jsonify(contests)
+
+@app.route('/api/codeforces/contests/', methods=['GET'])
+def get_codeforces_contests():
+    pass
+
+@app.route('/api/atcoder/contests/', methods=['GET'])
+def get_atcoder_contests():
+    pass
 
 if __name__ == '__main__':
     app.run()
---
+++ app/web_api/webscraping/codechef/script.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+from scrapingant_client import ScrapingAntClient
+
+# driver code for scraping
+def main():
+    # page url
+    URL = "https://www.codechef.com/contests/?itm_medium=navmenu&itm_campaign=allcontests#future-contests"
+    # scrapingant client
+    client = ScrapingAntClient(token='08195006585c4385956bd98ca77a3f07')
+    # sending req
+    res = client.general_request(URL)
+    # souping
+    page = res.content
+    soup = BeautifulSoup(page, 'lxml')
+    # print(soup)
+    # scraping
+    contests = {}
+    # present contests
+    present_contests = []
+    for row in soup.select('tbody#present-contests-data tr'):
+        details = []
+        for td in row:
+            detail = td.get_text()
+            details.append(detail)
+        contest = {}
+        contest['code'] = details[0]
+        contest['name'] = details[1]
+        contest['start'] = details[2]
+        contest['end'] = details[3]
+        present_contests.append(contest)
+    contests['present-contests'] = present_contests
+    # future contests
+    future_contests = []
+    for row in soup.select('tbody#future-contests-data tr'):
+        details = []
+        for td in row:
+            detail = td.get_text()
+            details.append(detail)
+        contest = {}
+        contest['code'] = details[0]
+        contest['name'] = details[1]
+        contest['start'] = details[2]
+        contest['end'] = details[3]
+        future_contests.append(contest)
+    contests['future-contests'] = future_contests
+    return contests
+
+if __name__ == '__main__':
+    main()
