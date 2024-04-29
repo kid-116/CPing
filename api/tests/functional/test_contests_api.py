3f69cad2b27b95ef98c8c1fb080bcb4b54edeef7
Mehul Todi <mehultodi116@gmail.com>
2024-04-29 18:35:21 +0530

    Test Firebase integration

--- api/tests/functional/test_contests_api.py
+++ api/tests/functional/test_contests_api.py
@@ -6,6 +6,8 @@ import dateutil.parser as dateutil_parser
 from flask.testing import FlaskClient
 import pytest
 
+from constants import Website
+
 
 def is_valid(body: list[dict[str, Any]]) -> bool:
     assert isinstance(body, list)
@@ -24,8 +26,8 @@ def is_valid(body: list[dict[str, Any]]) -> bool:
     return True
 
 
-@pytest.mark.parametrize('website', ['codeforces', 'atcoder'])
-def test_codeforces_contests_api(signed_client: FlaskClient,
+@pytest.mark.parametrize('website', [e.name.lower() for e in Website])
+def test_contests_api(signed_client: FlaskClient,
                                  website: str) -> None:
     response = signed_client.get(f'/api/contests/?website={website}')
     assert response.status_code == HTTPStatus.OK
---
+++ api/tests/functional/test_firebase_cache.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from flask.testing import FlaskClient
+import pytest
+
+from constants import Website
+
+
+@pytest.mark.parametrize('website', [e.name.lower() for e in Website])
+def test_firebase_integration(signed_client: FlaskClient,
+                                 website: str) -> None:
+    response = signed_client.get(f'/api/contests/?website={website}&cache')
+    assert response.status_code == HTTPStatus.OK
