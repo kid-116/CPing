15769694e747db892a7c4c6fef3b8eca98cf92e6
Mehul Todi <mehultodi116@gmail.com>
2024-05-11 14:17:19 +0530

    Test if app builds in CI

--- cping/lib/config/theme.dart
+++ cping/lib/config/theme.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 ThemeData darkTheme = ThemeData(
-  highlightColor: const Color(0xffee6c4d),
-  indicatorColor: const Color(0xff57e126),
-  colorScheme: ColorScheme.fromSwatch().copyWith(
-    background: const Color(0xff293241),
-    primary: const Color(0xff3d5a80),
-    onPrimary: Colors.white,
-    onSecondary: const Color(0xff293241),
-    secondary: const Color(0xff98c1d9), 
-    brightness: Brightness.dark,
-  ),
-  textTheme:TextTheme(
-    headline1: GoogleFonts.poppins(
+    highlightColor: const Color(0xffee6c4d),
+    indicatorColor: const Color(0xff57e126),
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      background: const Color(0xff293241),
+      primary: const Color(0xff3d5a80),
+      onPrimary: Colors.white,
+      onSecondary: const Color(0xff293241),
+      secondary: const Color(0xff98c1d9),
+      brightness: Brightness.dark,
+    ),
+    textTheme: TextTheme(
+        headline1: GoogleFonts.poppins(
       color: Colors.white,
       fontSize: 36,
       fontWeight: FontWeight.w800,
-    )
-  )
-);
+    )));
--- cping/lib/pages/website.dart
+++ cping/lib/pages/website.dart
@@ -10,15 +10,17 @@ Expanded listContests(List<Contest> contests, bool isOutDated) {
   return Expanded(
     child: Column(
       children: [
-        isOutDated ? const Center(
-            child: SizedBox(
-              height: 25,
-              width: 25,
-              child: CircularProgressIndicator(
-                strokeWidth: 2,
-                color: Colors.white,
-              ),
-            )) : Container(),
+        isOutDated
+            ? const Center(
+                child: SizedBox(
+                height: 25,
+                width: 25,
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                  color: Colors.white,
+                ),
+              ))
+            : Container(),
         SizedBox(
           height: isOutDated ? 10 : 0,
         ),
@@ -58,85 +60,82 @@ class _WebsitePageState extends State<WebsitePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          color: darkTheme.colorScheme.background,
-          child: Column(children: <Widget>[
-            Container(
-              padding: const  EdgeInsets.fromLTRB(24, 40, 30, 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                      onPressed: () => Navigator.of(context).pop(),
-                      icon: const Icon(Icons.arrow_back_ios, color: Colors.white)
+      color: darkTheme.colorScheme.background,
+      child: Column(children: <Widget>[
+        Container(
+          padding: const EdgeInsets.fromLTRB(24, 40, 30, 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  icon: const Icon(Icons.arrow_back_ios, color: Colors.white)),
+              Text(widget.name.toUpperCase(),
+                  style: darkTheme.textTheme.headline1)
+            ],
+          ),
+        ),
+        BlocConsumer<WebsiteBloc, WebsiteState>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            if (state is LoadingState) {
+              return const Padding(
+                padding: EdgeInsets.symmetric(vertical: 200, horizontal: 0),
+                child: Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.white,
+                )),
+              );
+            }
+            if (state is ErrorState) {
+              debugPrint(state.error.toString());
+              return const Expanded(
+                child: Center(
+                  child: Image(
+                    image: AssetImage('assets/images/error.png'),
+                    width: 250,
+                    color: Color.fromRGBO(255, 255, 255, 0.3),
+                    colorBlendMode: BlendMode.modulate,
+                    // color: Colors.white.withOpacity(0.3),
                   ),
-                  Text(
-                    widget.name.toUpperCase(),
-                    style: darkTheme.textTheme.headline1
-                  )
-                ],
+                ),
+              );
+            } else if (state is ContestsLoadedState) {
+              List<Contest> contests = state.contests;
+              if (state.isOutdated) {
+                BlocProvider.of<WebsiteBloc>(context)
+                    .add(RefreshContestsEvent());
+              }
+              return contests.isNotEmpty
+                  ? listContests(contests, state.isOutdated)
+                  : const Expanded(
+                      child: Center(
+                        child: Image(
+                          image: AssetImage('assets/images/empty.png'),
+                          width: 300,
+                          color: Color.fromRGBO(255, 255, 255, 0.3),
+                          colorBlendMode: BlendMode.modulate,
+                          // color: Colors.white.withOpacity(0.3),
+                        ),
+                      ),
+                    );
+            } else if (state is RefreshedCacheState) {
+              BlocProvider.of<WebsiteBloc>(context).add(GetContestsEvent());
+            }
+            return const Expanded(
+              child: Center(
+                child: Image(
+                  image: AssetImage('assets/images/error.png'),
+                  width: 250,
+                  color: Color.fromRGBO(255, 255, 255, 0.3),
+                  colorBlendMode: BlendMode.modulate,
+                  // color: Colors.white.withOpacity(0.3),
+                ),
               ),
-            ),
-            BlocConsumer<WebsiteBloc, WebsiteState>(
-              listener: (context, state) {},
-              builder: (context, state) {
-                if (state is LoadingState) {
-                  return const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 200, horizontal: 0),
-                    child: Center(
-                        child: CircularProgressIndicator(
-                      color: Colors.white,
-                    )),
-                  );
-                }
-                if (state is ErrorState) {
-                  debugPrint(state.error.toString());
-                  return const Expanded(
-                    child: Center(
-                      child: Image(
-                        image: AssetImage('assets/images/error.png'),
-                        width: 250,
-                        color: Color.fromRGBO(255, 255, 255, 0.3),
-                        colorBlendMode: BlendMode.modulate,
-                        // color: Colors.white.withOpacity(0.3),
-                      ),
-                    ),
-                  );
-                } else if (state is ContestsLoadedState) {
-                  List<Contest> contests = state.contests;
-                  if (state.isOutdated) {
-                    BlocProvider.of<WebsiteBloc>(context)
-                        .add(RefreshContestsEvent());
-                  }
-                  return contests.isNotEmpty
-                      ? listContests(contests, state.isOutdated)
-                      : const Expanded(
-                    child: Center(
-                      child: Image(
-                        image: AssetImage('assets/images/empty.png'),
-                        width: 300,
-                        color: Color.fromRGBO(255, 255, 255, 0.3),
-                        colorBlendMode: BlendMode.modulate,
-                        // color: Colors.white.withOpacity(0.3),
-                      ),
-                    ),
-                  );
-                } else if (state is RefreshedCacheState) {
-                  BlocProvider.of<WebsiteBloc>(context).add(GetContestsEvent());
-                }
-                return const Expanded(
-                  child: Center(
-                    child: Image(
-                      image: AssetImage('assets/images/error.png'),
-                      width: 250,
-                      color: Color.fromRGBO(255, 255, 255, 0.3),
-                      colorBlendMode: BlendMode.modulate,
-                      // color: Colors.white.withOpacity(0.3),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ]),
-        ));
+            );
+          },
+        ),
+      ]),
+    ));
   }
 }
